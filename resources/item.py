f8667f285df34c9fbce386625b5849f2ca665d27
Gilliam <Lexi.Gilliam@conocophillips.com>
2019-09-18 08:03:16 -0500

    added jwt_optional and fixed get

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
 from models.item import ItemModel
 
 
@@ -65,5 +65,13 @@ class Item(Resource):
 
 
 class ItemList(Resource):
+    @jwt_optional
     def get(self):
-        return {'items': [x.json() for x in ItemModel.find_all()]} #changed from a map so will run a little faster
+        user_id = get_jwt_identity()
+        items = [item.json() for item in ItemModel.find_all()]
+        if user_id:
+            return {'items': items}, 200
+        return {
+            'items': [item['name']] for item in items,
+            'message': 'more data available if you log in'
+        }, 200
